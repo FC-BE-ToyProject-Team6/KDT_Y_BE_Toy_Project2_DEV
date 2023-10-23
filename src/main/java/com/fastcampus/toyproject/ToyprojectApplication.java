0351ba02726265f4e6765c640258c41dcb7776f7
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-10-23 17:27:18 +0900

    [setting] first commit proejct setup

---
+++ src/main/java/com/fastcampus/toyproject/ToyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprojectApplication.class, args);
+	}
+
+}
