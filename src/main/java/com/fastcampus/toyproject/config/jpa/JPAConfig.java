9a7cfb981f27d04f8381e4f6a5a71aa40e07cbb4
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-10-24 15:43:56 +0900

    [refactor] JPAConfig.java 파일 위치 이동

--- src/main/java/com/fastcampus/toyproject/config/jpa/JPAConfig.java
+++ src/main/java/com/fastcampus/toyproject/config/jpa/JPAConfig.java
@@ -1,4 +1,4 @@
-package com.fastcampus.toyproject.common.config;
+package com.fastcampus.toyproject.config.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
