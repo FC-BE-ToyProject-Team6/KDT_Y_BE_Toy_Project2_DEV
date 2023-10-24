7f4b036627beb286d5c20b3da48d2a56cdba8476
hym <ghrltjdtprbs@gmail.com>
2023-10-24 17:44:14 +0900

    [refactor] code convetion적용

--- src/main/java/com/fastcampus/toyproject/domain/trip/dto/TripDTO.java
+++ src/main/java/com/fastcampus/toyproject/domain/trip/dto/TripDTO.java
@@ -10,9 +10,12 @@ import lombok.ToString;
 @Setter
 @ToString
 public class TripDTO {
+
     private Long memberId;
     private String tripName;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private boolean isDomestic;
+
+
 }
