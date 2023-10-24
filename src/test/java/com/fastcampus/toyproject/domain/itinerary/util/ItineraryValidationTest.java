1147417394f90d9a11c0f2d846e5d2b19e4302e2
sungjiwoon <nebulaisme@naver.com>
2023-10-24 13:51:50 +0900

    [feat] Itinerary order 검증메소드 테스트 코드 추가

---
+++ src/test/java/com/fastcampus/toyproject/domain/itinerary/util/ItineraryValidationTest.java
@@ -0,0 +1,61 @@
+package com.fastcampus.toyproject.domain.itinerary.util;
+
+import com.fastcampus.toyproject.common.exception.DefaultException;
+import com.fastcampus.toyproject.common.exception.ExceptionCode;
+import com.fastcampus.toyproject.domain.itinerary.dto.ItineraryDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.fastcampus.toyproject.common.exception.ExceptionCode.DUPLICATE_ITINERARY_OEDER;
+import static com.fastcampus.toyproject.common.exception.ExceptionCode.INCORRECT_ORDER;
+
+class ItineraryValidationTest {
+
+    LocalDateTime testStartDate, testEndDate;
+    List<ItineraryDTO> testList1, testList2;
+
+    public ItineraryValidationTest() {
+        this.testList1 = new ArrayList<>();
+        this.testList2 = new ArrayList<>();
+        this.testStartDate = LocalDateTime.now();
+        this.testEndDate = LocalDateTime.now();
+    }
+
+    @BeforeEach
+    void setUp() {
+        testList1.add(ItineraryDTO.builder().type(1).name("비행기").order(1).startDate(testStartDate).endDate(testEndDate).build());
+        testList1.add(ItineraryDTO.builder().type(2).name("호텔").order(2).startDate(testStartDate).endDate(testEndDate).build());
+        testList1.add(ItineraryDTO.builder().type(3).name("롯데월드").order(2).startDate(testStartDate).endDate(testEndDate).build());
+        testList1.add(ItineraryDTO.builder().type(2).name("호텔").order(3).startDate(testStartDate).endDate(testEndDate).build());
+
+        testList2.add(ItineraryDTO.builder().type(1).name("비행기").order(1).startDate(testStartDate).endDate(testEndDate).build());
+        testList2.add(ItineraryDTO.builder().type(2).name("호텔").order(10).startDate(testStartDate).endDate(testEndDate).build());
+        testList2.add(ItineraryDTO.builder().type(3).name("롯데월드").order(3).startDate(testStartDate).endDate(testEndDate).build());
+        testList2.add(ItineraryDTO.builder().type(2).name("호텔").order(4).startDate(testStartDate).endDate(testEndDate).build());
+    }
+
+    @Test
+    public void 순서가_제대로_검증이_되는지_확인() {
+        //given
+
+        //when 1) 중복 순서 검사
+        try {
+            ItineraryValidation.validateItinerariesOrder(testList1);
+        } catch (DefaultException e) {
+            Assertions.assertEquals(DUPLICATE_ITINERARY_OEDER, e.getErrorCode());
+        }
+
+        //when 2) 여정 번호 순서대로인지 검사
+        try {
+            ItineraryValidation.validateItinerariesOrder(testList2);
+        } catch (DefaultException e) {
+            Assertions.assertEquals(INCORRECT_ORDER, e.getErrorCode());
+        }
+    }
+
+}
