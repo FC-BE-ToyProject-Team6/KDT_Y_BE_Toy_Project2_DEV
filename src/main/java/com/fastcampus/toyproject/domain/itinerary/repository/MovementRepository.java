90214939ba5afefa29c2cd5a33d3547e6f3f2828
sungjiwoon <nebulaisme@naver.com>
2023-10-24 15:09:05 +0900

    [feat] Itinerary repository 생성 (여정, 이동, 숙박, 체류)

--- src/main/java/com/fastcampus/toyproject/domain/itinerary/repository/MovementRepository.java
+++ src/main/java/com/fastcampus/toyproject/domain/itinerary/repository/MovementRepository.java
@@ -1,9 +1,9 @@
 package com.fastcampus.toyproject.domain.itinerary.repository;
 
-import com.fastcampus.toyproject.domain.itinerary.entity.Itinerary;
+import com.fastcampus.toyproject.domain.itinerary.entity.Movement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
+public interface MovementRepository extends JpaRepository<Movement, Long> {
 }
