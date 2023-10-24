8751a9b65c2b8758f9e03e425beb00800a47fe32
sungjiwoon <nebulaisme@naver.com>
2023-10-24 12:39:37 +0900

    [feat] requestMapping 설정 (/itineraries)

--- src/main/java/com/fastcampus/toyproject/domain/itinerary/controller/ItineraryContoller.java
+++ src/main/java/com/fastcampus/toyproject/domain/itinerary/controller/ItineraryContoller.java
@@ -2,10 +2,12 @@ package com.fastcampus.toyproject.domain.itinerary.controller;
 
 import com.fastcampus.toyproject.domain.itinerary.service.ItineraryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/itineraries")
 public class ItineraryContoller {
     private final ItineraryService itineraryService;
 
