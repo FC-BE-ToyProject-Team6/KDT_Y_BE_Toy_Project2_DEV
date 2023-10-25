84bf1465402b7e0f3179953815e8835c79848214
sungjiwoon <nebulaisme@naver.com>
2023-10-25 12:05:13 +0900

    [feat] 여정 컨트롤러 수정 - 여정 삽입 추가

--- src/main/java/com/fastcampus/toyproject/domain/itinerary/controller/ItineraryController.java
+++ src/main/java/com/fastcampus/toyproject/domain/itinerary/controller/ItineraryController.java
@@ -1,14 +1,27 @@
 package com.fastcampus.toyproject.domain.itinerary.controller;
 
+import com.fastcampus.toyproject.common.dto.ResponseDTO;
+import com.fastcampus.toyproject.domain.itinerary.dto.ItineraryRequest;
+import com.fastcampus.toyproject.domain.itinerary.dto.ItineraryResponse;
 import com.fastcampus.toyproject.domain.itinerary.service.ItineraryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/itineraries")
 public class ItineraryController {
     private final ItineraryService itineraryService;
+
+    @PostMapping("/{tripId}")
+    public ResponseDTO<List<ItineraryResponse>> insertItineraries(
+            @PathVariable final Long tripId,
+            @Valid @RequestBody final List<ItineraryRequest> reqeust
+    ) {
+        return ResponseDTO.ok("여정들 삽입 완료", itineraryService.insertItineraries(tripId, reqeust));
+    }
 
 }
