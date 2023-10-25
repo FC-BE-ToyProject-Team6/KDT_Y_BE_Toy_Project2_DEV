69795b041b42f0793bf55d6c440d008ced588642
sungjiwoon <nebulaisme@naver.com>
2023-10-25 12:21:18 +0900

    [feat] 여정 응답 객체 잘 만들어지는지 테스트

---
+++ src/test/java/com/fastcampus/toyproject/domain/itinerary/dto/ItineraryResponseTest.java
@@ -0,0 +1,34 @@
+package com.fastcampus.toyproject.domain.itinerary.dto;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+class ItineraryResponseTest {
+    LocalDateTime testDate = LocalDateTime.now();
+
+    @Test
+    void 여정_응답_리스트_생성_테스트() {
+        List<ItineraryResponse> list = new ArrayList<>();
+        ItineraryResponse ir1 = MovementResponse.builder()
+                .itineraryName("비행기").itineraryOrder(1).departureDate(testDate).arrivalDate(testDate)
+                .departurePlace("인천").arrivalPlace("도쿄").build();
+        ItineraryResponse ir2 = LodgementResponse.builder()
+                .itineraryName("롯데호텔").itineraryOrder(2).checkIn(testDate).checkOut(testDate)
+                .build();
+        ItineraryResponse ir3 = StayResponse.builder()
+                .itineraryName("비행기").itineraryOrder(3).departureDate(testDate).arrivalDate(testDate)
+                .build();
+
+        list.add(ir1);
+        list.add(ir2);
+        list.add(ir3);
+
+
+    }
+
+
+}
