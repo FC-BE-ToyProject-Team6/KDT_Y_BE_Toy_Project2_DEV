71ef27af800799ab73bdde26c48feeaf31e9054d
sungjiwoon <nebulaisme@naver.com>
2023-10-25 10:06:23 +0900

    [feat] MemberRepository 추가

---
+++ src/main/java/com/fastcampus/toyproject/domain/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.fastcampus.toyproject.domain.member.repository;
+
+import com.fastcampus.toyproject.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
