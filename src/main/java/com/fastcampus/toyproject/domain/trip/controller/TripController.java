2fb842153f99ac845dac3084105d6a5a74d340a1
hym <ghrltjdtprbs@gmail.com>
2023-10-25 13:35:10 +0900

    [feat] memberid 추가 및 isDomestic 해결
    - 기존 null로 받던 memberid 값을 받을 수 있도록 수정했습니다.
    
    - isDomestic값이 변경 안되는 오류 해결했습니다

--- src/main/java/com/fastcampus/toyproject/domain/trip/controller/TripController.java
+++ src/main/java/com/fastcampus/toyproject/domain/trip/controller/TripController.java
@@ -5,34 +5,35 @@ import com.fastcampus.toyproject.common.dto.ResponseDTO;
 import com.fastcampus.toyproject.domain.trip.dto.TripDTO;
 import com.fastcampus.toyproject.domain.trip.entity.Trip;
 import com.fastcampus.toyproject.domain.trip.service.TripService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/trip")
+@RequestMapping("/api/trip/{memberId}")
 public class TripController {
 
-    private static final Logger log = LoggerFactory.getLogger(TripController.class);
 
     @Autowired
     private TripService tripService;
 
     @PostMapping
-    public ResponseEntity<ResponseDTO<Trip>> insertTrip(@RequestBody TripDTO tripDTO) {
-        Trip savedTrip = tripService.insertTrip(tripDTO);
+    public ResponseEntity<ResponseDTO<Trip>> insertTrip(@PathVariable Long memberId,
+        @RequestBody TripDTO tripDTO) {
+        Trip savedTrip = tripService.insertTrip(memberId, tripDTO);
         return ResponseEntity.ok(ResponseDTO.ok("Trip이 성공적으로 저장되었습니다.", savedTrip));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<ResponseDTO<Trip>> updateTrip(@PathVariable Long id,
+    @PutMapping("/{tripId}")
+    public ResponseEntity<ResponseDTO<Trip>> updateTrip(
+        @PathVariable Long memberId,
+        @PathVariable Long tripId,
         @RequestBody TripDTO tripDTO) {
-        Trip updatedTrip = tripService.updateTrip(id, tripDTO);
+        Trip updatedTrip = tripService.updateTrip(memberId, tripId, tripDTO);
         return ResponseEntity.ok(ResponseDTO.ok("Trip이 성공적으로 업데이트되었습니다.", updatedTrip));
     }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ResponseDTO<Void>> deleteTrip(@PathVariable Long id) {
