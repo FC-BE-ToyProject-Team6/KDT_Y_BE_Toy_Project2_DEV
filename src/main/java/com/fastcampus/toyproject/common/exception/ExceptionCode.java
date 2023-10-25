dce745d2a749ff3fe9d926ce174aea7ef949ddc1
hym <ghrltjdtprbs@gmail.com>
2023-10-25 13:33:32 +0900

    [refactor] 중복되는 exception메시지 리팩토링
    - 중복되는 exception 메시지 관리를 위해 리팩토링 진행했습니다.

--- src/main/java/com/fastcampus/toyproject/common/exception/ExceptionCode.java
+++ src/main/java/com/fastcampus/toyproject/common/exception/ExceptionCode.java
@@ -10,7 +10,9 @@ public enum ExceptionCode {
     DUPLICATE_ITINERARY_OEDER("여정 순서가 중복됩니다."),
     INCORRECT_ORDER("잘못된 여정 순서입니다."),
     INTERNAL_SERVER_ERROR("서버에 오류가 발생했습니다."),
-    INVALID_REQUEST("잘못된 요청입니다.")
+    INVALID_REQUEST("잘못된 요청입니다."),
+    NO_SUCH_TRIP("해당하는 Trip이 없습니다."),
+
     ;
 
     private String msg;
