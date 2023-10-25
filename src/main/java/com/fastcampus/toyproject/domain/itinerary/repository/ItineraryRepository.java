5f1eb8c385187c0643d6b28454f1aec48c4cf5b0
sungjiwoon <nebulaisme@naver.com>
2023-10-25 12:05:56 +0900

    [feat] 여정 레포지토리 수정 - 삽입

--- src/main/java/com/fastcampus/toyproject/domain/itinerary/repository/ItineraryRepository.java
+++ src/main/java/com/fastcampus/toyproject/domain/itinerary/repository/ItineraryRepository.java
@@ -1,9 +1,14 @@
 package com.fastcampus.toyproject.domain.itinerary.repository;
 
 import com.fastcampus.toyproject.domain.itinerary.entity.Itinerary;
+import com.fastcampus.toyproject.domain.trip.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
+    Optional<List<Itinerary>> findAllByTripId(Trip tripId);
 }
