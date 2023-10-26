c4325f9b7d6f99f8112466a69ccf67f5141a3399
hym <ghrltjdtprbs@gmail.com>
2023-10-26 14:25:57 +0900

    [refactor] Trip api변경
    - 편의를 위해 trip api를 변경하였습니다.

--- src/main/java/com/fastcampus/toyproject/domain/trip/controller/TripController.java
+++ src/main/java/com/fastcampus/toyproject/domain/trip/controller/TripController.java
@@ -2,8 +2,8 @@ package com.fastcampus.toyproject.domain.trip.controller;
 
 
 import com.fastcampus.toyproject.common.dto.ResponseDTO;
-import com.fastcampus.toyproject.domain.trip.dto.TripDTO;
-import com.fastcampus.toyproject.domain.trip.entity.Trip;
+import com.fastcampus.toyproject.domain.trip.dto.TripRequestDTO;
+import com.fastcampus.toyproject.domain.trip.dto.TripResponseDTO;
 import com.fastcampus.toyproject.domain.trip.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,33 +11,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/trip/{memberId}")
+@RequestMapping("/api/member/{memberId}/trip")
 public class TripController {
 
+    private final TripService tripService;
 
     @Autowired
-    private TripService tripService;
+    public TripController(TripService tripService) {
+        this.tripService = tripService;
+    }
 
     @PostMapping
-    public ResponseEntity<ResponseDTO<Trip>> insertTrip(@PathVariable Long memberId,
-        @RequestBody TripDTO tripDTO) {
-        Trip savedTrip = tripService.insertTrip(memberId, tripDTO);
+    public ResponseEntity<ResponseDTO<TripResponseDTO>> insertTrip(@PathVariable Long memberId,
+        @RequestBody TripRequestDTO tripRequestDTO) {
+        TripResponseDTO savedTrip = tripService.insertTrip(memberId, tripRequestDTO);
         return ResponseEntity.ok(ResponseDTO.ok("Trip이 성공적으로 저장되었습니다.", savedTrip));
     }
 
     @PutMapping("/{tripId}")
-    public ResponseEntity<ResponseDTO<Trip>> updateTrip(
+    public ResponseEntity<ResponseDTO<TripResponseDTO>> updateTrip(
         @PathVariable Long memberId,
         @PathVariable Long tripId,
-        @RequestBody TripDTO tripDTO) {
-        Trip updatedTrip = tripService.updateTrip(memberId, tripId, tripDTO);
+        @RequestBody TripRequestDTO tripRequestDTO) {
+        TripResponseDTO updatedTrip = tripService.updateTrip(memberId, tripId, tripRequestDTO);
         return ResponseEntity.ok(ResponseDTO.ok("Trip이 성공적으로 업데이트되었습니다.", updatedTrip));
     }
 
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ResponseDTO<Void>> deleteTrip(@PathVariable Long id) {
-        tripService.deleteTrip(id);
+    @DeleteMapping("/{tripId}")
+    public ResponseEntity<ResponseDTO<Void>> deleteTrip(@PathVariable Long tripId) {
+        tripService.deleteTrip(tripId);
         return ResponseEntity.ok(ResponseDTO.ok("Trip이 성공적으로 삭제되었습니다."));
     }
 }
