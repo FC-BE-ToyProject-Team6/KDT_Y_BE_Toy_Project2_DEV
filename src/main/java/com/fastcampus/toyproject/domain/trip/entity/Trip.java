d4886e8a5b23cb4e72d108134af79a1c25fc032f
sungjiwoon <nebulaisme@naver.com>
2023-10-27 10:28:33 +0900

    [fix] trip:itinerary=1:n 관계 수정에 따른 여정 리스트 추가

--- src/main/java/com/fastcampus/toyproject/domain/trip/entity/Trip.java
+++ src/main/java/com/fastcampus/toyproject/domain/trip/entity/Trip.java
@@ -1,11 +1,15 @@
 package com.fastcampus.toyproject.domain.trip.entity;
 
 import com.fastcampus.toyproject.common.BaseTimeEntity;
+import com.fastcampus.toyproject.domain.itinerary.entity.Itinerary;
 import com.fastcampus.toyproject.domain.member.entity.Member;
 import com.fastcampus.toyproject.domain.trip.dto.TripRequestDTO;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
@@ -15,6 +19,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -55,14 +60,17 @@ public class Trip extends BaseTimeEntity {
     @ColumnDefault("false")
     private boolean isDeleted;
 
+    @OneToMany(mappedBy = "trip", cascade = {CascadeType.ALL}, fetch = FetchType.EAGER, orphanRemoval = true)
+    List<Itinerary> itineraryList = new ArrayList<>();
+    //CascadeType.ALL -> 상위 객체 작업 하위객체 모두한테 전파.
+    //fetch = FetchType.EAGER -> 실제 조회할 때 한방 쿼리로 다 조회.(itinerary을 사용할 때 쿼리 안나가도 된다.)
+    //orphanRemoval = true -> 부모가 자식에 대한 참조를 끊을 때, 참조가 끊어진 자식 Entity(고아 객체)를 DB에서 삭제하도록 설정할 수 있다.
+    //우리는 soft delete 이므로 굳이 필요는 없음.
 
-    public void setIsDeleted(boolean isDeleted) {
-        this.isDeleted = isDeleted;
-    }
 
-    @Override
-    public void delete(LocalDateTime currentTime) {
-        super.delete(currentTime);
+    public void delete() {
+        super.delete(LocalDateTime.now());
+        this.isDeleted = true;
     }
 
     public void updateFromDTO(TripRequestDTO tripDTO) {
