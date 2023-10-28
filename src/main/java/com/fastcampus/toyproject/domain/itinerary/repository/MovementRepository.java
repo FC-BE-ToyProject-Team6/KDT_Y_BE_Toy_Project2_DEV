f404b0ecb458f69f77fb2717d76b5fe1bf411878
sungjiwoon <nebulaisme@naver.com>
2023-10-28 21:56:36 +0900

    [fix] 여정 레포지토리 수정

--- src/main/java/com/fastcampus/toyproject/domain/itinerary/repository/MovementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fastcampus.toyproject.domain.itinerary.repository;
-
-import com.fastcampus.toyproject.domain.itinerary.entity.Movement;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovementRepository extends JpaRepository<Movement, Long> {
-}
