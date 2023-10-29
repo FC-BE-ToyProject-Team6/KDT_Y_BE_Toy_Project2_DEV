f84793c157573d46e995a68141f71eafd46c300c
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-10-29 14:16:19 +0900

    [fix] Trip의 isDeleted false로직 추가

--- src/main/java/com/fastcampus/toyproject/domain/trip/repository/TripRepository.java
+++ src/main/java/com/fastcampus/toyproject/domain/trip/repository/TripRepository.java
@@ -2,10 +2,13 @@ package com.fastcampus.toyproject.domain.trip.repository;
 
 import com.fastcampus.toyproject.domain.trip.entity.Trip;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
 
     List<Trip> findAllByIsDeletedIsFalseOrderByTripId();
+
+    Optional<Trip> findByTripIdAndIsDeletedIsFalse(Long tripId);
 
 }
